f617156d32655fe2f0c34ed5b1745868bafab56d
NicholasFarrow <nicholas.w.farrow@gmail.com>
2021-01-22 23:01:31 +1100

    broken key variables in webhook

--- gateways/woo_webhook.py
+++ gateways/woo_webhook.py
@@ -6,7 +6,7 @@ import time
 import requests
 
 def hook(btcpyment_secret, payload, payment):
-    key = codecs.decode(secret, 'hex')
+    key = codecs.decode(btcpyment_secret, 'hex')
 
     # Calculate a secret that is required to send back to the
     # woocommerce gateway, proving we did not modify id nor amount.
@@ -18,7 +18,6 @@ def hook(btcpyment_secret, payload, payment):
     params = {"wc-api":"wc_btcpyment_gateway", 'id' : payload['id'], 'time' : str(paid_time)}
     message = (str(paid_time) + '.' + json.dumps(params, separators=(',', ':'))).encode('utf-8')
 
-    key = codecs.decode(btcpyment_secret, 'hex')
     hash = hmac.new(key, message, hashlib.sha256).hexdigest()
     headers={'Content-Type': 'application/json', 'X-Signature' : hash, 'X-Secret': secret}
 
