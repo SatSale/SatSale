f250c97f73fa3b6ce68d1f7e8b65601c69eb6fb1
NicholasFarrow <nicholas.w.farrow@gmail.com>
2021-02-04 23:18:27 +1100

    Handle concurrent lnd and btcd tunnels

--- ssh_tunnel.py
+++ ssh_tunnel.py
@@ -4,30 +4,43 @@ import config
 import invoice
 from pay import bitcoind
 
+def open_tunnel(host, port):
+    # If tunnel is required (might make things easier)
+    try:
+        if host is not None:
+            command = [
+                "ssh",
+                config.tunnel_host,
+                "-q",
+                "-N",
+                "-L",
+                "{}:localhost:{}".format(port, port),
+            ]
+            print("Opening tunnel to {}.".format(" ".join(command)))
+            tunnel_proc = subprocess.Popen(command)
+            return tunnel_proc
 
-# If tunnel is required (might make things easier)
-try:
-    if config.tunnel_host is not None:
-        command = [
-            "ssh",
-            config.tunnel_host,
-            "-q",
-            "-N",
-            "-L",
-            "{}:localhost:{}".format(config.rpcport, config.rpcport),
-        ]
-        print("Opening tunnel to {}.".format(" ".join(command)))
-        tunnel_proc = subprocess.Popen(command)
-    else:
+        else:
+            tunnel_proc = None
+    except Exception as e:
+        print("FAILED TO OPEN TUNNEL. Exception: {}".format(e))
         tunnel_proc = None
-except Exception as e:
-    print("FAILED TO OPEN TUNNEL. Exception: {}".format(e))
-    tunnel_proc = None
-    pass
-
+        pass
+    return
 
 def close_tunnel():
     if tunnel_proc is not None:
         tunnel_proc.kill()
         print("Tunnel closed.")
     return
+
+# Open tunnel
+if config.tunnel_host is not None:
+    tunnel_proc = open_tunnel(config.tunnel_host, config.rpcport)
+
+    # Also for lnd if enabled
+    if 'lnd_rpcport' in config.__dict__.keys():
+        open_tunnel(config.tunnel_host, config.lnd_rpcport)
+
+else:
+    tunnel_proc = None
