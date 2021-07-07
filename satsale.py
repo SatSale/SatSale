c4d0c41e3d510d23ab5c3ce6c0c01295f5e78e9e
Nick <nicholas.w.farrow@gmail.com>
2021-07-08 03:59:10 +1000

    satsale v2 (#12) Refactored code into REST API, client side processing. Created API docs and removed old code. Speeed
    * SatSale v2 - Refactored code into REST API, client side processing. Created API docs and removed old code. Speeed
    
    * Fix werkzeug dep
    
    * Fix js checkpayment json bug
    
    * fix api responses for /checkpayment

--- satsale.py
+++ satsale.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, request, redirect
-from flask_socketio import SocketIO, emit
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    Blueprint,
+    make_response,
+    url_for,
+)
+from flask_restplus import Resource, Api, Namespace, fields
 import time
 import os
+import uuid
+import sqlite3
+from pprint import pprint
+import json
 
 import ssh_tunnel
 import config
@@ -9,12 +21,11 @@ import invoice
 from pay import bitcoind
 from pay import lnd
 from gateways import woo_webhook
+from invoice.price_feed import get_btc_value
 
-# Begin websocket
-async_mode = None
 app = Flask(__name__)
 
-# Load an API key or create a new one
+# Load a SatSale API key or create a new one
 if os.path.exists("SatSale_API_key"):
     with open("SatSale_API_key", "r") as f:
         app.config["SECRET_KEY"] = f.read().strip()
@@ -25,187 +36,272 @@ else:
 
 print("Initialised Flask with secret key: {}".format(app.config["SECRET_KEY"]))
 
-# cors_allowed_origins * allows for webhooks to be initiated from iframes.
-socket_ = SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
-
-
-# Basic return on initialisation
-@socket_.on("initialise")
-def test_message(message):
-    emit(
-        "payresponse",
-        {
-            "status": "Initialising payment...",
-            "time_left": 0,
-            "response": "Initialising payment...",
-        },
-    )
+# Create payment database if it does not exist
+if not os.path.exists("database.db"):
+    with sqlite3.connect("database.db") as conn:
+        print("Creating new database.db...")
+        conn.execute(
+            "CREATE TABLE payments (uuid TEXT, dollar_value DECIMAL, btc_value DECIMAL, method TEXT, address TEXT, time DECIMAL, webhook TEXT, rhash TEXT)"
+        )
 
 
 # Render index page
 # This is currently a donation page that submits to /pay
 @app.route("/")
 def index():
-    # Render donation page
-    return render_template("donate.html", async_mode=socket_.async_mode)
+    headers = {"Content-Type": "text/html"}
+    return make_response(render_template("donate.html"), 200, headers)
 
 
-# /pay is the main payment method for initiating a payment websocket.
+# /pay is the main page for initiating a payment, takes a GET request with ?amount=
 @app.route("/pay")
-def payment_page():
+def pay():
     params = dict(request.args)
-    params['lnd_enabled'] = (config.pay_method == "lnd")
+    params["lnd_enabled"] = config.pay_method == "lnd"
+    params["redirect"] = config.redirect
     # Render payment page with the request arguments (?amount= etc.)
-    return render_template("index.html", params=params, async_mode=socket_.async_mode)
+    headers = {"Content-Type": "text/html"}
+    return make_response(render_template("index.html", params=params), 200, headers)
 
 
-# Websocket payment processing method called by client
-# initiate_payment recieves amount and initiates invoice and payment processing.
-@socket_.on("initiate_payment")
-def initiate_payment(payload):
-    # Check the amount is a float
-    amount = payload["amount"]
-    try:
-        amount = float(amount)
-    except Exception as e:
-        update_status(payment, "Invalid amount.")
-        amount = None
-        return
+# Now we build the API docs
+# (if you do this before the above @app.routes then / gets overwritten.)
+api = Api(
+    app,
+    version="0.1",
+    title="SatSale API",
+    default="SatSale /api/",
+    description="API for creating Bitcoin invoices and processing payments.",
+    doc="/docs/",
+    order=True,
+)
 
-    # Label as a donation if the id is missing
-    if "id" in payload.keys():
-        label = payload["id"]
-    else:
-        label = "donation"
+# Model templates for API responses
+invoice_model = api.model(
+    "invoice",
+    {
+        "uuid": fields.String(),
+        "dollar_value": fields.Float(),
+        "btc_value": fields.Float(),
+        "method": fields.String(),
+        "address": fields.String(),
+        "time": fields.Float(),
+        "webhook": fields.String(),
+        "rhash": fields.String(),
+        "time_left": fields.Float(),
+    },
+)
+status_model = api.model(
+    "status",
+    {
+        "payment_complete": fields.Integer(),
+        "confirmed_paid": fields.Float(),
+        "unconfirmed_paid": fields.Float(),
+        "expired": fields.Integer(),
+    },
+)
 
-    # Get payment method, use one specified in query string if provided
-    if "method" in payload.keys():
-            payment_method = payload['method']
-    else:
-        payment_method = config.pay_method
 
-    # Initialise the payment invoice
-    payment = create_invoice(amount, "USD", label, payment_method)
+@api.doc(
+    params={
+        "amount": "An amount in USD.",
+        "method": "(Optional) Specify a payment method: `bitcoind` for onchain, `lnd` for lightning).",
+        "w_url": "(Optional) Specify a webhook url to call after successful payment. Currently only supports WooCommerce plugin.",
+    }
+)
+class create_payment(Resource):
+    @api.response(200, "Success", invoice_model)
+    @api.response(400, "Invalid payment method")
+    def get(self):
+        "Create Payment"
+        """Initiate a new payment with an `amount` in USD."""
+        dollar_amount = request.args.get("amount")
+        currency = "USD"
+        label = ""  # request.args.get('label')
+        payment_method = request.args.get("method")
+        if payment_method is None:
+            payment_method = config.pay_method
+        webhook = request.args.get("w_url")
+        if webhook is None:
+            webhook = None
 
-    # Wait for the amount to be sent to the address
-    process_payment(payment)
+        # Create the payment using one of the connected nodes as a base
+        # ready to recieve the invoice.
+        node = get_node(payment_method)
+        if node is None:
+            print("Invalid payment method {}".format(payment_method))
+            return {"message": "Invalid payment method."}, 400
 
-    # On successful payment
-    if payment.paid:
-        update_status(payment, "Payment finalised. Thankyou!")
+        invoice = {
+            "uuid": str(uuid.uuid4().hex),
+            "dollar_value": dollar_amount,
+            "btc_value": round(get_btc_value(dollar_amount, currency), 8),
+            "method": payment_method,
+            "time": time.time(),
+            "webhook": webhook,
+        }
 
-        # If a w_url for woocommerce webhook has been provided, then we need
-        # to take some additional steps to confirm the order.
-        if "w_url" in payload.keys():
+        invoice["address"], invoice["rhash"] = node.get_address(
+            invoice["btc_value"], invoice["uuid"]
+        )
+        node.create_qr(invoice["uuid"], invoice["address"], invoice["btc_value"])
+
+        with sqlite3.connect("database.db") as conn:
+            cur = conn.cursor()
+            cur.execute(
+                "INSERT INTO payments (uuid,dollar_value,btc_value,method,address,time,webhook,rhash) VALUES (?,?,?,?,?,?,?,?)",
+                (
+                    invoice["uuid"],
+                    invoice["dollar_value"],
+                    invoice["btc_value"],
+                    invoice["method"],
+                    invoice["address"],
+                    invoice["time"],
+                    invoice["webhook"],
+                    invoice["rhash"],
+                ),
+            )
+
+        invoice["time_left"] = config.payment_timeout - (time.time() - invoice["time"])
+        print("Created invoice:")
+        pprint(invoice)
+        print()
+
+        return {"invoice": invoice}, 200
+
+
+@api.doc(params={"uuid": "A payment uuid. Received from /createpayment."})
+class check_payment(Resource):
+    @api.response(200, "Success", status_model)
+    @api.response(201, "Unconfirmed", status_model)
+    @api.response(202, "Payment Expired", status_model)
+    def get(self):
+        "Check Payment"
+        """Check the status of a payment."""
+        uuid = request.args.get("uuid")
+        status = check_payment_status(uuid)
+
+        response = {
+            "payment_complete": 0,
+            "confirmed_paid": 0,
+            "unconfirmed_paid": 0,
+            "expired": 0,
+        }
+
+        if status["time_left"] <= 0:
+            response.update({"expired": 1})
+            code = 202
+        else:
+            # Don't send paid amounts if payment is expired.
+            response.update(status)
+
+        if status['payment_complete'] == 1:
+            code = 200
+        else:
+            code = 201
+        
+        return {'status': response}, code
+
+
+@api.doc(params={"uuid": "A payment uuid. Received from /createpayment."})
+class complete_payment(Resource):
+    @api.response(200, "Payment confirmed.")
+    @api.response(400, "Payment expired.")
+    @api.response(500, "Webhook failure.")
+    def get(self):
+        "Complete Payment"
+        """Run post-payment processing such as any webhooks."""
+        uuid = request.args.get("uuid")
+
+        invoice = load_invoice_from_db(uuid)
+        status = check_payment_status(uuid)
+
+        if status["time_left"] < 0:
+            return {"expired"}, 400
+
+        if (invoice["webhook"] != None) and (invoice["webhook"] != ""):
             # Call webhook
-            response = woo_webhook.hook(app.config["SECRET_KEY"], payload, payment)
+            response = woo_webhook.hook(app.config["SECRET_KEY"], invoice)
 
             if response.status_code != 200:
-                print(
-                    "Failed to confirm order payment via webhook {}, please contact the store to ensure the order has been confirmed, error response is: {}".format(
-                        response.status_code, response.text
-                    )
+                err = "Failed to confirm order payment via webhook {}, please contact the store to ensure the order has been confirmed, error response is: {}".format(
+                    response.status_code, response.text
                 )
-                update_status(payment, response.text)
-            else:
-                print("Successfully confirmed payment via webhook.")
-                update_status(payment, "Order confirmed.")
+                print(err)
+                return {"message": err}, 500
 
-        # Redirect after payment
-        # TODO: add a delay here. Test.
-        if config.redirect is not None:
-            print("Redirecting to {}".format(config.redirect))
-            return redirect(config.redirect)
-        else:
-            print("No redirect, closing.")
+            print("Successfully confirmed payment via webhook.")
+            return {"message": "Payment confirmed with store."}, 200
 
-    return
+        return {"message": "Payment confirmed."}, 200
 
 
-# Return feedback via the websocket, updating the payment status and time remaining.
-def update_status(payment, status, console_status=True):
-    payment.status = status
-    payment.response = status
-    # Log to python stdout also
-    if console_status:
-        print(payment.status)
-
-    # Send status & response to client
-    emit(
-        "payresponse",
-        {
-            "status": payment.status,
-            "address": payment.address,
-            "amount": payment.value,
-            "time_left": payment.time_left,
-            "uuid": payment.uuid,
-            "response": payment.response,
-        },
-    )
-    return
-
-
-# Initialise the payment via the payment method (bitcoind / lightningc / etc),
-def create_invoice(dollar_amount, currency, label, payment_method=config.pay_method):
-    if payment_method == "bitcoind":
-        payment = bitcoin_node
-    elif payment_method == "lnd":
-        payment = lightning_node
+def check_payment_status(uuid):
+    status = {}
+    invoice = load_invoice_from_db(uuid)
+    if invoice is None:
+        status.update({"time_left": 0, "not_found": 1})
     else:
-        print("Invalid payment method")
-        return
+        status["time_left"] = config.payment_timeout - (time.time() - invoice["time"])
 
-    # Load invoice information
-    payment.invoice(dollar_amount, currency, label)
-
-    return payment
-
-
-# Payment processing function.
-# Handle payment logic.
-def process_payment(payment):
-    update_status(payment, "Payment intialised, awaiting payment.")
-
-    # Track start_time so we can detect payment timeouts
-    payment.start_time = time.time()
-    while (config.payment_timeout - (time.time() - payment.start_time)) > 0:
-        # Not using := for compatability reasons..
-        payment.time_left = config.payment_timeout - (time.time() - payment.start_time)
-        # Check progress of the payment
-        payment.confirmed_paid, payment.unconfirmed_paid = payment.check_payment()
-        print()
-        print(payment.__dict__)
+    if status["time_left"] > 0:
+        node = get_node(invoice["method"])
+        if invoice["method"] == "lnd":
+            conf_paid, unconf_paid = node.check_payment(invoice["rhash"])
+        else:
+            conf_paid, unconf_paid = node.check_payment(invoice["address"])
 
         # Debugging and demo mode which auto confirms payments after 5 seconds
-        dbg_free_mode_cond = config.free_mode and (time.time() - payment.start_time > 5)
+        dbg_free_mode_cond = config.free_mode and (time.time() - invoice["time"] > 5)
         # If payment is paid
-        if (payment.confirmed_paid > payment.value) or dbg_free_mode_cond:
-            payment.paid = True
-            payment.time_left = 0
-            update_status(
-                payment, "Payment successful! {} BTC".format(payment.confirmed_paid)
+        if (conf_paid > invoice["btc_value"]) or dbg_free_mode_cond:
+            status.update(
+                {
+                    "payment_complete": 1,
+                    "confirmed_paid": conf_paid,
+                    "unconfirmed_paid": unconf_paid,
+                }
             )
-            break
-
-        # Display unconfirmed transaction info
-        elif payment.unconfirmed_paid > 0:
-            update_status(
-                payment,
-                "Discovered payment. \
-                Waiting for {} confirmations...".format(
-                    config.required_confirmations
-                ),
-                console_status=False,
-            )
-            socket_.sleep(config.pollrate)
-
-        # Continue waiting for transaction...
         else:
-            update_status(payment, "Waiting for payment...")
-            socket_.sleep(config.pollrate)
+            status.update(
+                {
+                    "payment_complete": 0,
+                    "confirmed_paid": conf_paid,
+                    "unconfirmed_paid": unconf_paid,
+                }
+            )
+
+    print("Invoice {} status: {}".format(uuid, status))
+    return status
+
+
+def get_node(payment_method):
+    if payment_method == "bitcoind":
+        node = bitcoin_node
+    elif payment_method == "lnd":
+        node = lightning_node
     else:
-        update_status(payment, "Payment expired.")
-    return
+        node = None
+    return node
+
+
+def load_invoice_from_db(uuid):
+    with sqlite3.connect("database.db") as conn:
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        rows = cur.execute(
+            "select * from payments where uuid='{}'".format(uuid)
+        ).fetchall()
+    if len(rows) > 0:
+        return [dict(ix) for ix in rows][0]
+    else:
+        return None
+
+
+# Add API endpoints
+api.add_resource(create_payment, "/api/createpayment")
+api.add_resource(check_payment, "/api/checkpayment")
+api.add_resource(complete_payment, "/api/completepayment")
 
 
 # Test connections on startup:
@@ -218,4 +314,4 @@ if config.pay_method == "lnd":
 
 
 if __name__ == "__main__":
-    socket_.run(app, debug=False)
+    app.run(debug=False)
