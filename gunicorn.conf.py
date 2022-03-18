a40539910585bbc961bcd5642ef9b178b30f7a42
nickfarrow <nick@nickfarrow.com>
2022-03-19 03:46:57 +1100

    gunicorn conf controls ssh processes

---
+++ gunicorn.conf.py
@@ -0,0 +1,11 @@
+from gateways import ssh_tunnel
+
+def on_starting(server):
+    server.ssh_processes = ssh_tunnel.open_tunnels()
+
+def on_reload(server):
+    ssh_tunnel.close_tunnels(server.ssh_processes)
+    server.ssh_processes = ssh_tunnel.open_tunnels()
+
+def worker_exit(server, worker):
+    ssh_tunnel.close_tunnels(server.ssh_processes)
