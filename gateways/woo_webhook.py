8f10bf0c4cbaa9edd1f3d6287b60dabcee5399a5
nickfarrow <nick@nickfarrow.com>
2022-08-10 22:16:54 +1000

    rename fiat_currency to base_currency
    Avoids 'fiat_currency':'BTC'
    
    Co-authored-by: Kristaps Kaupe <kristaps@blogiem.lv>

--- gateways/woo_webhook.py
+++ gateways/woo_webhook.py
@@ -12,7 +12,7 @@ def hook(satsale_secret, invoice, order_id):
 
     # Calculate a secret that is required to send back to the
     # woocommerce gateway, proving we did not modify id nor amount.
-    secret_seed = str(int(100 * float(invoice["fiat_value"]))).encode("utf-8")
+    secret_seed = str(int(100 * float(invoice["base_value"]))).encode("utf-8")
     logging.info("Secret seed: {}".format(secret_seed))
 
     secret = hmac.new(key, secret_seed, hashlib.sha256).hexdigest()
