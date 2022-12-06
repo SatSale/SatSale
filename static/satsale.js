SatSale/SatSale

commit 56b674552fcd56438328c281a351c76a1455153e

Towards universal QR codes
* Move Bitcoin / QR code generation code out of individual nodes + add
  tests;
* Base class for all node classes;
* Split off bolt11_invoice from onchain address in payments table (db);
* More type hints.

static/satsale.js
@@ -13,7 +13,12 @@ function payment(payment_data) {
             invoice = data.invoice;
             payment_uuid = invoice.uuid;
 
-            $('#address').text(invoice.address).html();
+            if (invoice.payment_method == 'lightning') {
+                $('#address').text(invoice.bolt11_invoice).html();
+            }
+            else {
+                $('#address').text(invoice.address).html();
+            }
             $('#amount').text(invoice.btc_value).html();
             $('#amount_sats').text(Math.round(invoice.btc_value * 10**8)).html();
             $('#timer').text(Math.round(invoice.time_left)).html();

node/bitcoind.py
@@ -1,20 +1,18 @@
 import json
 import logging
 import os
-import qrcode
 import requests
 import time
 from typing import Tuple
 
 import config
-from node.bip21 import encode_bip21_uri
-from utils import btc_amount_format
+from node import node
 
 
-class btcd:
+class bitcoind(node.node):
+
     def __init__(self, node_config: dict) -> None:
-        self.config = node_config
-        self.is_onchain = True
+        super().__init__(node_config, True)
 
         if self.config['tor_bitcoinrpc_host'] is not None:
             from gateways.tor import session
@@ -53,9 +51,7 @@ class btcd:
                 )
 
             try:
-                info = self.call_bitcoin_rpc("getblockchaininfo")
-
-                logging.info(info)
+                logging.info(self.get_info())
                 logging.info("Successfully contacted bitcoind.")
                 break
 
@@ -76,7 +72,10 @@ class btcd:
                 Check your RPC / port tunneling settings and try again."
             )
 
-    def call_bitcoin_rpc(self, method: str, params: list = None) -> dict:
+    def get_info(self):
+        return self._call_bitcoin_rpc("getblockchaininfo")
+
+    def _call_bitcoin_rpc(self, method: str, params: list = None) -> dict:
         payload = json.dumps({"method": method, "params": params})
         logging.debug(payload)
         headers = {
@@ -98,17 +97,8 @@ class btcd:
             raise RuntimeError("Bitcoin RPC failed: {}".format(response_data["error"]))
         return response_data["result"]
 
-    def create_qr(self, uuid: str, address: str, value: float) -> None:
-        qr_str = encode_bip21_uri(address, {
-            "amount": btc_amount_format(value),
-            "label": uuid
-        })
-        img = qrcode.make(qr_str)
-        img.save("static/qr_codes/{}.png".format(uuid))
-        return
-
     def check_payment(self, uuid: str) -> Tuple[float, float]:
-        transactions = self.call_bitcoin_rpc("listtransactions", [uuid])
+        transactions = self._call_bitcoin_rpc("listtransactions", [uuid])
 
         conf_paid = 0
         unconf_paid = 0
@@ -121,12 +111,12 @@ class btcd:
         return conf_paid, unconf_paid
 
     def get_address(self, amount: float, label: str,
-                    expiry: int) -> Tuple[str, str]:
+                    expiry: int) -> Tuple[str, str, str]:
         for i in range(config.connection_attempts):
             try:
-                address = self.call_bitcoin_rpc("getnewaddress", [label])
+                address = self._call_bitcoin_rpc("getnewaddress", [label])
 
-                return address, None
+                return address, None, None
 
             except Exception as e:
                 logging.error(e)

node/clightning.py
@@ -1,8 +1,9 @@
 import time
-import qrcode
 import logging
+from typing import Tuple
 
 import config
+from node import node
 
 # if False:  # config.tor_clightningrpc_host is not None:
 #     from gateways.tor import session
@@ -12,12 +13,12 @@ import config
 #     session = None
 
 
-class clightning:
-    def __init__(self, node_config):
+class clightning(node.node):
+
+    def __init__(self, node_config: dict):
         from pyln.client import LightningRpc
 
-        self.config = node_config
-        self.is_onchain = False
+        super().__init__(node_config, False)
 
         for i in range(config.connection_attempts):
             try:
@@ -59,22 +60,16 @@ class clightning:
         logging.info("Ready for payments requests.")
         return
 
-    def create_qr(self, uuid, address, value):
-        qr_str = "{}".format(address.upper())
-        img = qrcode.make(qr_str)
-        img.save("static/qr_codes/{}.png".format(uuid))
-        return
-
     def get_info(self):
         return self.clightning.getinfo()
 
-    def get_uri(self):
+    def get_uri(self) -> str:
         info = self.get_info()
         address = info["address"][0]
         return info["id"] + "@" + address["address"] + ":" + str(address["port"])
 
     # Create lightning invoice
-    def create_clightning_invoice(self, btc_amount, label, expiry):
+    def _create_clightning_invoice(self, btc_amount, label, expiry):
         # Multiplying by 10^8 to convert to satoshi units
         msats_amount = int(float(btc_amount) * 10 ** (3 + 8))
         lnd_invoice = self.clightning.invoice(
@@ -82,12 +77,13 @@ class clightning:
         )
         return lnd_invoice["bolt11"], lnd_invoice["payment_hash"]
 
-    def get_address(self, amount, label, expiry):
-        address, r_hash = self.create_clightning_invoice(amount, label, expiry)
-        return address, r_hash
+    def get_address(self, amount: float, label: str,
+                    expiry: int) -> Tuple[str, str, str]:
+        address, r_hash = self._create_clightning_invoice(amount, label, expiry)
+        return None, address, r_hash
 
     # Check whether the payment has been paid
-    def check_payment(self, uuid):
+    def check_payment(self, uuid: str) -> Tuple[float, float]:
         invoices = self.clightning.listinvoices(uuid)["invoices"]
 
         if len(invoices) == 0:

node/invoices.py
@@ -1,0 +1,39 @@
+import qrcode
+from enum import Enum
+
+from node.bip21 import encode_bip21_uri
+from utils import btc_amount_format
+
+
+class InvoiceType(Enum):
+    BIP21 = 1,
+    BOLT11 = 2
+
+
+def encode_bitcoin_invoice(uuid: str, invoice: dict,
+                           invtype: InvoiceType) -> str:
+
+    if invtype == InvoiceType.BIP21:
+        assert (invoice["address"])
+        assert (invoice["btc_value"])
+        bip21_params = {
+            "amount": btc_amount_format(invoice["btc_value"]),
+            "label": uuid
+        }
+        if "bolt11_invoice" in invoice and invoice["bolt11_invoice"]:
+            bip21_params["lightning"] = invoice["bolt11_invoice"]
+        return encode_bip21_uri(invoice["address"], bip21_params)
+
+    elif invtype == InvoiceType.BOLT11:
+        assert (invoice["bolt11_invoice"])
+        return invoice["bolt11_invoice"]
+
+    else:
+        raise NotImplementedError(
+            "Unsupported Bitcoin invoice type {}".format(invtype))
+
+
+def create_qr(uuid: str, qr_str: str) -> None:
+    img = qrcode.make(qr_str)
+    img.save("static/qr_codes/{}.png".format(uuid))
+    return

node/lnd.py
@@ -4,21 +4,22 @@ import os
 import json
 from base64 import b64decode
 from google.protobuf.json_format import MessageToJson
-import qrcode
-import logging
+from typing import Tuple
 
 import config
+import logging
+from node import node
 
 
-class lnd:
-    def __init__(self, node_config):
+class lnd(node.node):
+
+    def __init__(self, node_config: dict) -> None:
         from lndgrpc import LNDClient
 
-        self.config = node_config
-        self.is_onchain = False
+        super().__init__(node_config, False)
 
         # Copy admin macaroon and tls cert to local machine
-        self.copy_certs()
+        self._copy_certs()
 
         # Conect to lightning node
         connection_str = "{}:{}".format(config.host, self.config['lnd_rpcport'])
@@ -69,14 +70,8 @@ class lnd:
         logging.info("Ready for payments requests.")
         return
 
-    def create_qr(self, uuid, address, value):
-        qr_str = "{}".format(address.upper())
-        img = qrcode.make(qr_str)
-        img.save("static/qr_codes/{}.png".format(uuid))
-        return
-
     # Copy tls and macaroon certs from remote machine.
-    def copy_certs(self):
+    def _copy_certs(self) -> None:
         self.certs = {"tls": "tls.cert", "macaroon": self.config['lnd_macaroon']}
 
         if (not os.path.isfile("tls.cert")) or (
@@ -124,7 +119,9 @@ class lnd:
         return
 
     # Create lightning invoice
-    def create_lnd_invoice(self, btc_amount, memo=None, description_hash=None, expiry=3600):
+    def _create_lnd_invoice(self, btc_amount: float, memo: str = None,
+                            description_hash: str = None,
+                            expiry: int = 3600) -> Tuple[str, str]:
         # Multiplying by 10^8 to convert to satoshi units
         sats_amount = int(float(btc_amount) * 10 ** 8)
         res = self.lnd.add_invoice(
@@ -134,14 +131,15 @@ class lnd:
 
         return lnd_invoice["paymentRequest"], lnd_invoice["rHash"]
 
-    def get_address(self, amount, label, expiry):
+    def get_address(self, amount: float, label: str,
+                    expiry: int) -> Tuple[str, str, str]:
         address, r_hash = self.create_lnd_invoice(
             amount, memo=label, expiry=expiry)
-        return address, r_hash
+        return None, address, r_hash
 
-    def pay_invoice(self, invoice):
+    def pay_invoice(self, bolt11_invoice: str) -> None:
         ret = json.loads(
-            MessageToJson(self.lnd.send_payment(invoice, fee_limit_msat=20 * 1000))
+            MessageToJson(self.lnd.send_payment(bolt11_invoice, fee_limit_msat=20 * 1000))
         )
         logging.info(ret)
         return
@@ -149,12 +147,12 @@ class lnd:
     def get_info(self):
         return json.loads(MessageToJson(self.lnd.get_info()))
 
-    def get_uri(self):
+    def get_uri(self) -> str:
         info = self.get_info()
         return info["uris"][0]
 
     # Check whether the payment has been paid
-    def check_payment(self, rhash):
+    def check_payment(self, rhash: str) -> Tuple[float, float]:
         invoice_status = json.loads(
             MessageToJson(self.lnd.lookup_invoice(r_hash_str=b64decode(rhash).hex()))
         )

node/lndhub.py
@@ -1,18 +1,18 @@
 import json
 import logging
-import qrcode
 import time
 from typing import Tuple
 
 import config
+from node import node
 
 
-class lndhub:
+class lndhub(node.node):
+
     def __init__(self, node_config: dict) -> None:
         from blue_wallet_client import BlueWalletClient
 
-        self.config = node_config
-        self.is_onchain = False
+        super().__init__(node_config, False)
 
         for i in range(config.connection_attempts):
             try:
@@ -46,12 +46,6 @@ class lndhub:
     def get_info(self) -> dict:
         return self.lndhub.get_node_info()
 
-    def create_qr(self, uuid: str, address: str, value: float) -> None:
-        qr_str = "{}".format(address.upper())
-        img = qrcode.make(qr_str)
-        img.save("static/qr_codes/{}.png".format(uuid))
-        return
-
     def create_lndhub_invoice(self, btc_amount: float, memo: str = None,
                               expiry: int = 3600) -> Tuple[str, str]:
         sats_amount = int(float(btc_amount) * 10 ** 8)
@@ -59,8 +53,10 @@ class lndhub:
         return ret["payment_request"], ret["r_hash"]
 
     def get_address(self, btc_amount: float, label: str,
-                    expiry: int) -> Tuple[str, str]:
-        return self.create_lndhub_invoice(btc_amount, label, expiry)
+                    expiry: int) -> Tuple[str, str, str]:
+        address, r_hash = self.create_lndhub_invoice(
+            btc_amount, label, expiry)
+        return None, address, r_hash
 
     def pay_invoice(self, invoice: str) -> None:
         try:

node/node.py
@@ -1,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+
+class node(ABC):
+
+    def __init__(self, node_config: dict, is_onchain: bool) -> None:
+        self.config = node_config
+        self.is_onchain = is_onchain
+
+    @abstractmethod
+    def get_info(self):
+        pass
+
+    @abstractmethod
+    def get_address(self, amount: float, label: str,
+                    expiry: int) -> Tuple[str, str, str]:
+        pass
+
+    @abstractmethod
+    def check_payment(self, uuid: str) -> Tuple[float, float]:
+        pass

node/xpub.py
@@ -1,5 +1,4 @@
 import logging
-import qrcode
 import requests
 import sys
 import time
@@ -9,8 +8,7 @@ from bip_utils import \
     Bip86, Bip86Coins
 from typing import Tuple
 
-from node.bip21 import encode_bip21_uri
-from utils import btc_amount_format
+from node import node
 from payments import database
 
 
@@ -22,11 +20,11 @@ class InvalidExtendedPublicKeyError(RuntimeError):
         super().__init__(self.message)
 
 
-class xpub:
+class xpub(node.node):
 
     def __init__(self, node_config: dict) -> None:
-        self.is_onchain = True
-        self.config = node_config
+
+        super().__init__(node_config, True)
 
         if "pytest" not in sys.modules:
             next_n = self.get_next_address_index(self.config["xpub"])
@@ -47,14 +45,8 @@ class xpub:
                 self.config["api_url"]))
             logging.info("Next address shown to users is #{}".format(next_n))
 
-    def create_qr(self, uuid: str, address: str, value: float) -> None:
-        qr_str = encode_bip21_uri(address, {
-            "amount": btc_amount_format(value),
-            "label": uuid
-        })
-        img = qrcode.make(qr_str)
-        img.save("static/qr_codes/{}.png".format(uuid))
-        return
+    def get_info(self):
+        return self.config["api_url"]
 
     def check_payment(self, address: str, slow: bool = True) -> Tuple[float, float]:
         conf_paid, unconf_paid = 0, 0
@@ -125,7 +117,8 @@ class xpub:
         address = child_key.PublicKey().ToAddress()
         return address
 
-    def get_address(self, amount: float, label: str, expiry: int) -> Tuple[str, str]:
+    def get_address(self, amount: float, label: str,
+                    expiry: int) -> Tuple[str, str, str]:
         while True:
             n = self.get_next_address_index(self.config["xpub"])
             address = self.get_address_at_index(n)
@@ -133,4 +126,4 @@ class xpub:
             conf_paid, unconf_paid = self.check_payment(address, slow=False)
             if conf_paid == 0 and unconf_paid == 0:
                 break
-        return address, None
+        return address, None, None

commit e55af9d95addc0018f272965038f4d233f590ba5

Add back copyable LN invoice string at checkout page
It was broken by 56b674552fcd56438328c281a351c76a1455153e, only QR code
was shown for LN.

static/satsale.js
@@ -13,7 +13,7 @@ function payment(payment_data) {
             invoice = data.invoice;
             payment_uuid = invoice.uuid;
 
-            if (invoice.payment_method == 'lightning') {
+            if (invoice.method == 'lightning') {
                 $('#address').text(invoice.bolt11_invoice).html();
             }
             else {